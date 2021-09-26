44899a8744c08eb30ee685d519d39bf81752d4ec
Yuhao Feng <flyhaozi@live.com>
2021-09-26 17:50:49 +0800

    Add svg icons

--- pages/[project].js
+++ pages/[project].js
@@ -30,21 +30,21 @@ export default function Project({ source, frontMatter }) {
             <div className={cstyles.top}>
                 <h1>{frontMatter.title}</h1>
                 <p>{frontMatter.description}</p>
-                <p>
+                <div>
                     {frontMatter.links.map((link, index) => {
                         return <a href={link} target="_blank" key={index}>{
                             link.startsWith("https://chrome.google.com") &&
-                            <img src="https://camo.githubusercontent.com/a4857075aa233365548b863664816a8d15172d5aa5dc5ca5d7e2abfebfb09152/68747470733a2f2f73746f726167652e676f6f676c65617069732e636f6d2f6368726f6d652d6763732d75706c6f616465722e61707073706f742e636f6d2f696d6167652f576c443877433667386b685957504a5573516365516b6858536c76312f55563443347962654254735a74343355347869732e706e67" alt="Install from Chrome Web Store" />
+                            <img src="/icons/chrome-web-store.svg" alt="Install from Chrome Web Store" />
                         } {link.startsWith("https://microsoftedge.microsoft.com") &&
-                            <img src="https://user-images.githubusercontent.com/12277417/132962008-04e2971d-c769-41df-9adc-08a089295def.png" alt="Get it from Microsoft" />
+                            <img src="/icons/microsoft-store.svg" alt="Get it from Microsoft" />
                             } {link.startsWith("https://greasyfork.org") &&
-                                <img src="https://greasyfork.org/packs/media/images/blacklogo96-b2384000fca45aa17e45eb417cbcbb59.png" alt="Install from Greasy Fork" height="58" />
+                                <img src="/icons/greasyfork.svg" alt="Install from Greasy Fork" />
                             } {link.startsWith("https://github.com") &&
-                                <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="Github" height="58" />
+                                <img src="/icons/github-white.svg" alt="View on Github" />
                             }
                         </a>
                     })}
-                </p>
+                </div>
             </div>
             <section className={cstyles.section}>
                 <MDXRemote {...source} components={ components } />
