340053cf79c0bcfea2c330f00e58a51813adc139
Yuhao Feng <flyhaozi@live.com>
2021-09-26 21:31:27 +0800

    Improve head meta

--- pages/blog.js
+++ pages/blog.js
@@ -1,11 +1,18 @@
+import Head from 'next/head'
+import Link from 'next/link'
 import cstyles from '../styles/Common.module.css'
 
 export default function Blog() {
     return (
         <section className={cstyles.section}>
+            <Head >
+                <title>Blog | fHz</title>
+                <meta property="og:title" content="Blog | fHz" key="title" />
+                <meta property="og:url" content="https://flyhaozi.com/blog" key="url" />
+            </Head>
             <h2>Blog (Under Construction)</h2>
             <ul className={cstyles.blogList}>
-            <li><span>2021-10-01</span><a href="/blog/2021-national-day">Celebrate The 2021 National Day!!! 🎉✨</a></li>
+                <li><span>2021-10-01</span><Link href="/blog/2021-national-day"><a >Celebrate The 2021 National Day!!! 🎉✨</a></Link></li>
             </ul>
         </section>
     )
