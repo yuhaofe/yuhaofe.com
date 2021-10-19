428585ecd783e7f2ce94f7abc2a93880cfe66882
Yuhao Feng <git@yuhaofe.com>
2021-10-20 03:11:41 +0800

    Optimize meta tags and add nosnippet

--- components/footer.js
+++ components/footer.js
@@ -2,15 +2,17 @@ import styles from '../styles/Layout.module.css'
 
 export default function Footer({ texts }) {
     return (
-        <footer className={styles.footer}>
-            <span dangerouslySetInnerHTML={{__html: texts.copyright}}></span>
-            <div className={styles.tools}>
-                <span dangerouslySetInnerHTML={{__html: texts.designed}}></span>
-                <span dangerouslySetInnerHTML={{__html: texts.built}}></span>
-                <span dangerouslySetInnerHTML={{__html: texts.hosted}}></span>
-            </div>
-            <div className={styles.social}>
-                <span dangerouslySetInnerHTML={{__html: texts.follow}}></span>
+        <footer style={{width: '100%'}}>
+            <div className={styles.footer} data-nosnippet>
+                <span dangerouslySetInnerHTML={{__html: texts.copyright}}></span>
+                <div className={styles.tools}>
+                    <span dangerouslySetInnerHTML={{__html: texts.designed}}></span>
+                    <span dangerouslySetInnerHTML={{__html: texts.built}}></span>
+                    <span dangerouslySetInnerHTML={{__html: texts.hosted}}></span>
+                </div>
+                <div className={styles.social}>
+                    <span dangerouslySetInnerHTML={{__html: texts.follow}}></span>
+                </div>
             </div>
         </footer>
     )
